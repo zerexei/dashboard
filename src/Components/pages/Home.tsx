130416c71fa3cef74b32cef4c3b5ac1b9d05b358
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-12 13:11:20 +0800

    Remove react module

---
+++ src/Components/pages/Home.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome user!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, iste
+        eaque quidem maxime dolore mollitia debitis quas beatae possimus porro!
+        Deleniti repellat iste aliquam nostrum inventore fugiat totam sint
+        corrupti.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
