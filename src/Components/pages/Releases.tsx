a9aafa4f308f017016a8cb8a50b09bb3584afeca
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-13 13:23:51 +0800

    Add user events

--- src/Components/pages/Releases.tsx
+++ src/Components/pages/Releases.tsx
@@ -1,9 +1,20 @@
+import axios from "axios";
 import React from "react";
+import config from "../../.config";
 
 const Releases = () => {
+  const handleClick = async () => {
+    const res = await axios.get("https://api.github.com/users/zerexei/events", {
+      headers: {
+        Authorization: `token ${config.api_key}`,
+      },
+    });
+    console.log(res);
+  };
   return (
     <div>
       <h2>Releases</h2>
+      <button onClick={handleClick}>Events</button>
     </div>
   );
 };
