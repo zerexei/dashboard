d2016ec909f936077346cee6165e4ed522575df5
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-13 09:37:00 +0800

    Remove p tag

--- src/Components/NavUser.tsx
+++ src/Components/NavUser.tsx
@@ -5,10 +5,10 @@ const NavUser = () => {
     <div>
       <div className="flex items-center gap-2 mb-4">
         <div className="p-3 border">Logo</div>
-        <p className="flex items-center p-2 cursor-pointer hover:bg-gray-200">
-          <h1>Lorem Ipsum</h1>
+        <h2 className="flex items-center p-2 cursor-pointer hover:bg-gray-200">
+          Lorem Ipsum
           <ChevronDownIcon className="h-5 w-5"></ChevronDownIcon>
-        </p>
+        </h2>
       </div>
     </div>
   );
