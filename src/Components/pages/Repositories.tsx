10efb7996794dc7934f996dbc6431013d0fcd0ce
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-13 14:18:32 +0800

    extract all repos

--- src/Components/pages/Repositories.tsx
+++ src/Components/pages/Repositories.tsx
@@ -32,25 +32,25 @@ const Repositories = () => {
     setRepos(await res.data);
   };
 
+  const allRepos = sortByUpdatedAt(repos).map((repo) => {
+    return (
+      <div key={repo.id} className="w-1/5 h-40 p-6 bg-gray-50">
+        <a
+          href={repo.html_url}
+          className="text-blue-400 hover:text-blue-500 hover:underline"
+        >
+          {repo.name}
+        </a>
+        <p className="mt-4 text-sm text-gray-900">
+          {repo?.description || "No description available"}
+        </p>
+      </div>
+    );
+  });
+
   return (
     <div className="flex flex-wrap justify-center gap-2">
-      {repos.length === 0
-        ? "No current repository"
-        : sortByUpdatedAt(repos).map((repo) => {
-            return (
-              <div key={repo.id} className="w-1/5 h-40 p-6 bg-gray-50">
-                <a
-                  href={repo.html_url}
-                  className="text-blue-400 hover:text-blue-500 hover:underline"
-                >
-                  {repo.name}
-                </a>
-                <p className="mt-4 text-sm text-gray-900">
-                  {repo?.description || "No description available"}
-                </p>
-              </div>
-            );
-          })}
+      {repos.length > 0 ? allRepos : "No current repository"}
     </div>
   );
 };
