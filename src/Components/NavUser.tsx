1a748d70eee3dcfca8daffe27ef03a6c144532e9
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-13 12:11:59 +0800

    Setup axios and user avatar

--- src/Components/NavUser.tsx
+++ src/Components/NavUser.tsx
@@ -1,10 +1,37 @@
+import axios from "axios";
 import { ChevronDownIcon } from "@heroicons/react/solid";
+import { useEffect, useState } from "react";
+
+type UserProps = {
+  avatar_url: string;
+};
 
 const NavUser = () => {
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const fetchedUser = await axios.get(
+          "https://api.github.com/users/zerexei"
+        );
+        setUser(fetchedUser.data);
+      } catch (error) {
+        console.trace(error);
+      }
+    })();
+  });
+
   return (
     <div>
       <div className="flex items-center gap-2 mb-4">
-        <div className="p-3 border">Logo</div>
+        <div className="w-12 h-12">
+          <img
+            src={user.avatar_url}
+            alt="User avatar"
+            className="object-cover rounded"
+          />
+        </div>
         <h2 className="flex items-center p-2 cursor-pointer hover:bg-gray-200">
           Lorem Ipsum
           <ChevronDownIcon className="h-5 w-5"></ChevronDownIcon>
--- src/Components/pages/Wiki.tsx
+++ src/Components/pages/Wiki.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import axios from "axios";
+import config from "../../.config";
 
 const Wiki = () => {
+  const handleClick = async () => {
+    const res = await axios.get("https://api.github.com/users/zerexei", {
+      headers: {
+        Authorization: `token ${config.api_key}`,
+      },
+    });
+    console.log(res);
+  };
+
   return (
     <div>
       <h2>Wiki</h2>
+      <button onClick={handleClick}>Button</button>
     </div>
   );
 };
