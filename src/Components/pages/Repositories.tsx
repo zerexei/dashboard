23283af9d9a5a9756387238dc1d895cb7e19a605
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-13 13:54:03 +0800

    extract to method & change type RepoProps to interface

--- src/Components/pages/Repositories.tsx
+++ src/Components/pages/Repositories.tsx
@@ -1,14 +1,14 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
 import config from "../../.config";
 
-type RepoProps = {
+export interface RepoProps {
   id: number;
   html_url: string;
   name: string;
   description: string;
   updated_at: string;
-};
+}
 
 const sortByUpdatedAt = (items: RepoProps[]) =>
   items.sort((a, b) => (b["updated_at"] > a["updated_at"] ? 1 : -1));
@@ -17,27 +17,28 @@ const Repositories = () => {
   const [repos, setRepos] = useState<RepoProps[]>([] as RepoProps[]);
 
   useEffect(() => {
-    (async () => {
-      const res = await axios.get(
-        "https://api.github.com/users/zerexei/repos?page=1&per_page=12",
-        {
-          headers: {
-            Authorization: `token ${config.api_key}`,
-          },
-        }
-      );
-      setRepos(await res.data);
-    })();
-  }, [repos]);
+    getRepos();
+  }, []);
+
+  const getRepos = async () => {
+    const res = await axios.get(
+      "https://api.github.com/users/zerexei/repos?page=1&per_page=12",
+      {
+        headers: {
+          Authorization: `token ${config.api_key}`,
+        },
+      }
+    );
+    setRepos(await res.data);
+  };
 
   return (
     <div className="flex flex-wrap justify-center gap-2">
       {repos &&
         sortByUpdatedAt(repos).map((repo) => {
           return (
-            <div className="w-1/5 h-40 p-6 bg-gray-50">
+            <div key={repo.id} className="w-1/5 h-40 p-6 bg-gray-50">
               <a
-                key={repo.id}
                 href={repo.html_url}
                 className="text-blue-400 hover:text-blue-500 hover:underline"
               >
