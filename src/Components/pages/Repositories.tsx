819696e70b2fa985cb54b652196ef03830e2ff95
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-13 14:14:36 +0800

    Add catch block & no repo use feedback

--- src/Components/pages/Repositories.tsx
+++ src/Components/pages/Repositories.tsx
@@ -4,9 +4,9 @@ import config from "../../.config";
 
 export interface RepoProps {
   id: number;
-  html_url: string;
   name: string;
   description: string;
+  html_url: string;
   updated_at: string;
 }
 
@@ -17,7 +17,7 @@ const Repositories = () => {
   const [repos, setRepos] = useState<RepoProps[]>([] as RepoProps[]);
 
   useEffect(() => {
-    getRepos();
+    getRepos().catch(console.dir);
   }, []);
 
   const getRepos = async () => {
@@ -34,22 +34,23 @@ const Repositories = () => {
 
   return (
     <div className="flex flex-wrap justify-center gap-2">
-      {repos &&
-        sortByUpdatedAt(repos).map((repo) => {
-          return (
-            <div key={repo.id} className="w-1/5 h-40 p-6 bg-gray-50">
-              <a
-                href={repo.html_url}
-                className="text-blue-400 hover:text-blue-500 hover:underline"
-              >
-                {repo.name}
-              </a>
-              <p className="mt-4 text-sm text-gray-900">
-                {repo?.description || "No description available"}
-              </p>
-            </div>
-          );
-        })}
+      {repos.length === 0
+        ? "No current repository"
+        : sortByUpdatedAt(repos).map((repo) => {
+            return (
+              <div key={repo.id} className="w-1/5 h-40 p-6 bg-gray-50">
+                <a
+                  href={repo.html_url}
+                  className="text-blue-400 hover:text-blue-500 hover:underline"
+                >
+                  {repo.name}
+                </a>
+                <p className="mt-4 text-sm text-gray-900">
+                  {repo?.description || "No description available"}
+                </p>
+              </div>
+            );
+          })}
     </div>
   );
 };
