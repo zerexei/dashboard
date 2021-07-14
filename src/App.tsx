556db8685007ba7ca8a18691d48b704b8d163904
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-14 16:25:14 +0800

    Add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavBar from "./Components/NavBar";
@@ -14,37 +15,41 @@ import Stats from "./Components/pages/Stats";
 import Trendy from "./Components/pages/Trendy";
 import Wiki from "./Components/pages/Wiki";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen max-h-screen p-6 bg-gray-50">
-        <div className="flex flex-1 items-stretch bg-white shadow-md ">
-          {/* ======================LEFT================= */}
-          <div className="w-1/6 p-2 text-white bg-green-700 rounded-l">
-            <NavUser />
-            <NavBar />
-          </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="flex flex-col min-h-screen max-h-screen p-6 bg-gray-50">
+          <div className="flex flex-1 items-stretch bg-white shadow-md ">
+            {/* ======================LEFT================= */}
+            <div className="w-1/6 p-2 text-white bg-green-700 rounded-l">
+              <NavUser />
+              <NavBar />
+            </div>
 
-          {/* =================RIGHT==================== */}
-          <div className="flex-1 p-2 rounded-r">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/dashboard" component={Dashboard} />
-              <Route exact path="/repos" component={Repositories} />
-              <Route exact path="/repos/:name" component={Repository} />
-              <Route exact path="/trendy" component={Trendy} />
-              <Route exact path="/gists" component={Gists} />
+            {/* =================RIGHT==================== */}
+            <div className="flex-1 p-2 rounded-r">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/repos" component={Repositories} />
+                <Route exact path="/repos/:name" component={Repository} />
+                <Route exact path="/trendy" component={Trendy} />
+                <Route exact path="/gists" component={Gists} />
 
-              <Route exact path="/issues" component={Issues} />
-              <Route exact path="/pull-request" component={PullRequest} />
-              <Route exact path="/releases" component={Releases} />
-              <Route exact path="/wiki" component={Wiki} />
-              <Route exact path="/stats" component={Stats} />
-            </Switch>
+                <Route exact path="/issues" component={Issues} />
+                <Route exact path="/pull-request" component={PullRequest} />
+                <Route exact path="/releases" component={Releases} />
+                <Route exact path="/wiki" component={Wiki} />
+                <Route exact path="/stats" component={Stats} />
+              </Switch>
+            </div>
           </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/Components/pages/Gists.tsx
+++ src/Components/pages/Gists.tsx
@@ -1,5 +1,5 @@
+import { useQuery } from "react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
 import config from "../../.config";
 
 export interface GistProps {
@@ -8,30 +8,38 @@ export interface GistProps {
 }
 
 const Gists = () => {
-  const [gists, setGists] = useState<GistProps[]>([] as GistProps[]);
+  const { isLoading, error, data } = useQuery<GistProps[], Error>(
+    "gists",
+    /**
+     * Fetch all public gists
+     */
+    async () => {
+      const result = await axios.get("https://api.github.com/gists/public", {
+        headers: {
+          Authorization: `token ${config.api_key}`,
+        },
+      });
+      return await result.data;
+    }
+  );
 
-  useEffect(() => {
-    getAllGists();
-  }, []);
-
-  async function getAllGists() {
-    const res = await axios.get("https://api.github.com/gists/public", {
-      headers: {
-        Authorization: `token ${config.api_key}`,
-      },
-    });
-    setGists(await res.data);
-  }
-
-  const allGists = gists.map((gist) => (
-    <a key={gist.id} href={gist.html_url} className="hover:text-blue-400">
-      {gist.html_url}
-    </a>
-  ));
+  if (isLoading) return <p>"Loading..."</p>;
+  if (error) return <p>{`An error has occured: ${error.message}`}</p>;
+  if (!data || data.length <= 0) return <p>No current gist available</p>;
 
   return (
     <div className="flex flex-col gap-5">
-      {gists.length > 0 ? allGists : "No current gist."}
+      {data.map((gist: GistProps) => (
+        <a
+          key={gist.id}
+          href={gist.html_url}
+          className="hover:text-blue-400"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {gist.html_url}
+        </a>
+      ))}
     </div>
   );
 };
